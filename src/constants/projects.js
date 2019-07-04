1447f83cd492614523b6816a3a3428157bf9b33c
jdecroock <decroockjovi@gmail.com>
2019-07-04 18:47:53 +0200

    (chore) - correct image sizes

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,7 +1,7 @@
-import thumbRozebeke from '../assets/images/thumbs/cohousingrozebeke-1.png'
-import thumbUrbanDesign from '../assets/images/thumbs/urbandesign-1.png'
-import thumbWeststation from '../assets/images/thumbs/weststation-1.png'
-import thumb04 from '../assets/images/thumbs/04.jpg'
+import thumbRozebeke from '../assets/images/thumbs/rozebeke.jpg'
+import thumbUrbanDesign from '../assets/images/thumbs/urbandesign.jpg'
+import thumbWeststation from '../assets/images/thumbs/weststation.jpg'
+import thumbHotelPalace from '../assets/images/thumbs/hotelpalace.jpg'
 
 import fullRozebeke from '../assets/images/fulls/cohousingrozebeke-1.png'
 import fullUrbanDesign from '../assets/images/fulls/urbandesign-1.png'
@@ -12,5 +12,5 @@ export default [
   { id: 'Rozebeke', src: fullRozebeke, thumbnail: thumbRozebeke, caption: 'Cohousing Rozebeke (2016)', description: 'TODO'},
   { id: 'UrbanDesign', src: fullUrbanDesign, thumbnail: thumbUrbanDesign, caption: 'Urban design (2017)', description: 'TODO'},
   { id: 'Weststation', src: fullWeststation, thumbnail: thumbWeststation, caption: 'Weststation (2018)', description: 'TODO'},
-  { id: 'HotelPalace', src: full04, thumbnail: thumb04, caption: 'Hotel Palace (2019)', description: 'TODO'}
+  { id: 'HotelPalace', src: full04, thumbnail: thumbHotelPalace, caption: 'Hotel Palace (2019)', description: 'TODO'}
 ];
