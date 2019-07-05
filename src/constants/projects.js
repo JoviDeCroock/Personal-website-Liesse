964355288d20a6293de71c02e0152f6f78b9b1b4
jdecroock <decroockjovi@gmail.com>
2019-07-05 15:01:58 +0200

    (chore) - add rozebeke pictures

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -5,7 +5,10 @@ import west2 from '../assets/images/fulls/weststation/2.jpg'
 import west3 from '../assets/images/fulls/weststation/3.jpg'
 // Rozebeke
 import thumbRozebeke from '../assets/images/thumbs/rozebeke.jpg'
-import fullRozebeke from '../assets/images/fulls/rozebeke/cohousingrozebeke-1.png'
+import rozebeke1 from '../assets/images/fulls/rozebeke/1.jpg'
+import rozebeke2 from '../assets/images/fulls/rozebeke/2.jpg'
+import rozebeke3 from '../assets/images/fulls/rozebeke/3.jpg'
+import rozebeke4 from '../assets/images/fulls/rozebeke/4.jpg'
 // Hotel Palace
 import thumbHotelPalace from '../assets/images/thumbs/hotelpalace.jpg'
 import hotel3 from '../assets/images/fulls/hotelpalace/3.jpg'
@@ -19,7 +22,7 @@ import urban3 from '../assets/images/fulls/urbandesign/3.jpg'
 import urban4 from '../assets/images/fulls/urbandesign/4.jpg'
 
 export default [
-  { id: 'Rozebeke', additionalImages: [{ src: fullRozebeke }], thumbnail: thumbRozebeke, caption: 'Cohousing Rozebeke', description: '2016' },
+  { id: 'Rozebeke', additionalImages: [{ src: rozebeke1 }, { src: rozebeke2 }, { src: rozebeke3 }, { src: rozebeke4 }], thumbnail: thumbRozebeke, caption: 'Cohousing Rozebeke', description: '2016' },
   { id: 'UrbanDesign', additionalImages: [{ src: urban1 }, { src: urban2 }, { src: urban3 }, { src: urban4 }], thumbnail: thumbUrbanDesign, caption: 'Urban design', description: '2017' },
   { id: 'Weststation', additionalImages: [{src: west1 }, {src: west2 }, {src: west3 },], thumbnail: thumbWeststation, caption: 'Weststation', description: '2018' },
   { id: 'HotelPalace', additionalImages: [{ src: hotel3 }, { src: hotel4 }, { src: hotel5 }], thumbnail: thumbHotelPalace, caption: 'Hotel Palace', description: '2019' }
