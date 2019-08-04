b6b3c9b0f5abcbb17f0f45212cdf3383731afacf
Jovi De Croock <jovi.decroock@formidable.com>
2019-08-04 15:26:14 +0100

    (chore) - optimise images

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,25 +1,25 @@
 // Weststation
-import thumbWeststation from '../assets/images/thumbs/weststation.jpg'
-import west1 from '../assets/images/fulls/weststation/1.jpg'
-import west2 from '../assets/images/fulls/weststation/2.jpg'
-import west3 from '../assets/images/fulls/weststation/3.jpg'
+import thumbWeststation from '../assets/images/thumbs/weststation.webp'
+import west1 from '../assets/images/fulls/weststation/1.webp'
+import west2 from '../assets/images/fulls/weststation/2.webp'
+import west3 from '../assets/images/fulls/weststation/3.webp'
 // Rozebeke
-import thumbRozebeke from '../assets/images/thumbs/rozebeke.jpg'
-import rozebeke1 from '../assets/images/fulls/rozebeke/1.jpg'
-import rozebeke2 from '../assets/images/fulls/rozebeke/2.jpg'
-import rozebeke3 from '../assets/images/fulls/rozebeke/3.jpg'
-import rozebeke4 from '../assets/images/fulls/rozebeke/4.jpg'
+import thumbRozebeke from '../assets/images/thumbs/rozebeke.webp'
+import rozebeke1 from '../assets/images/fulls/rozebeke/1.webp'
+import rozebeke2 from '../assets/images/fulls/rozebeke/2.webp'
+import rozebeke3 from '../assets/images/fulls/rozebeke/3.webp'
+import rozebeke4 from '../assets/images/fulls/rozebeke/4.webp'
 // Hotel Palace
-import thumbHotelPalace from '../assets/images/thumbs/hotelpalace.jpg'
-import hotel3 from '../assets/images/fulls/hotelpalace/3.jpg'
-import hotel4 from '../assets/images/fulls/hotelpalace/4.jpg'
-import hotel5 from '../assets/images/fulls/hotelpalace/5.jpg'
+import thumbHotelPalace from '../assets/images/thumbs/hotelpalace.webp'
+import hotel3 from '../assets/images/fulls/hotelpalace/3.webp'
+import hotel4 from '../assets/images/fulls/hotelpalace/4.webp'
+import hotel5 from '../assets/images/fulls/hotelpalace/5.webp'
 // Urban Design
-import thumbUrbanDesign from '../assets/images/thumbs/urbandesign.jpg'
-import urban1 from '../assets/images/fulls/urbandesign/1.jpg'
-import urban2 from '../assets/images/fulls/urbandesign/2.jpg'
-import urban3 from '../assets/images/fulls/urbandesign/3.jpg'
-import urban4 from '../assets/images/fulls/urbandesign/4.jpg'
+import thumbUrbanDesign from '../assets/images/thumbs/urbandesign.webp'
+import urban1 from '../assets/images/fulls/urbandesign/1.webp'
+import urban2 from '../assets/images/fulls/urbandesign/2.webp'
+import urban3 from '../assets/images/fulls/urbandesign/3.webp'
+import urban4 from '../assets/images/fulls/urbandesign/4.webp'
 
 export default [
   { id: 'Rozebeke', additionalImages: [{ src: thumbRozebeke }, { src: rozebeke1 }, { src: rozebeke2 }, { src: rozebeke3 }, { src: rozebeke4 }], thumbnail: thumbRozebeke, caption: 'Cohousing Rozebeke', description: '2016' },
