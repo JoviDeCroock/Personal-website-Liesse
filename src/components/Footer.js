a063074e0d949aa20d259f5daf681d4541e69d97
Jovi De Croock <jovi.decroock@formidable.com>
2019-08-04 15:29:38 +0100

    (chore) - improve accessibility

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,12 @@ const Footer = () => (
     <div id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="mailto:liesse.swinnen@gmail.com" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                <li>
+                    <a
+                        aria-label="Mail Liesse Swinnen"
+                        href="mailto:liesse.swinnen@gmail.com"
+                        className="icon fa-envelope-o"
+                    ><span className="label">Email</span></a></li>
             </ul>
             <ul className="copyright">
                 <li>&copy; Liesse Swinnen</li>
